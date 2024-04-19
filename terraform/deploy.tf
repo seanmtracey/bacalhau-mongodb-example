b59f7380b42b8d868c41182288c4d5e31947d09a
seanmtracey <seanmtracey@gmail.com>
2024-04-19 01:19:27 +0100

    Remove outdated user data script;

--- terraform/deploy.tf
+++ terraform/deploy.tf
@@ -98,49 +98,6 @@ resource "google_compute_instance" "my_instance" {
 		ssh-keys  = "${var.username}:${file(var.public_key)}",
 	}
 
-	/*metadata_startup_script = <<-EOF
-		#!/bin/bash
-
-		# Update package list and install necessary packages
-		apt-get update -y
-		apt-get install -y python3 python3-pip docker.io unzip
-
-		curl -sL https://get.bacalhau.org/install.sh | bash
-
-		bacalhau serve
-
-		# Pull MongoDB Docker image
-		docker pull mongo:latest
-
-		# Run MongoDB container
-		docker run --name local-mongo --restart=always -d -p 27017:27017 -v mongo_data:/data/db mongo:latest
-
-		# Download and extract files
-		wget https://storage.googleapis.com/${google_storage_bucket_object.zip_file.bucket}/${google_storage_bucket_object.zip_file.name}
-		unzip ./${google_storage_bucket_object.zip_file.name} -d ./scripts
-		cd ./scripts
-
-		# Install Python dependencies
-		pip3 install --no-cache-dir -r requirements.txt
-
-		# Create systemd service file
-		echo -e '[Unit]\nDescription=The system poller for use with MongoDB\nAfter=network.target\n\n[Service]\nType=simple\nExecStart=/usr/bin/python3 /scripts/main.py\nRestart=always\n\n[Install]\nWantedBy=multi-user.target' > ~/poller.service
-
-		# Move systemd service file to appropriate directory
-		sudo mv ~/poller.service /etc/systemd/system/
-
-		# Reload systemd daemon
-		sudo systemctl daemon-reload
-
-		# Enable and start the systemd service
-		sudo systemctl enable poller.service
-		sudo systemctl start poller.service
-
-		# Enable Docker service
-		sudo systemctl enable docker.service
-
-	EOF*/
-
 }
 
 
